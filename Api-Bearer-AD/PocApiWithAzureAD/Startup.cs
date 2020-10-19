02e248f8ccf3f90d6c3cfa9ed6338b0f058fc213
Thiago Cruz <thiago.cruz@xpi.com.br>
2020-10-19 18:58:57 -0300

    Add jwt options

--- Api-Bearer-AD/PocApiWithAzureAD/Startup.cs
+++ Api-Bearer-AD/PocApiWithAzureAD/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,12 @@ namespace PocApiWithAzureAD
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(o =>
+                {
+                    o.Audience = Configuration["AAD:ResourseId"];
+                    o.Authority = $"{Configuration["AAD:InstanceId"]}{Configuration["AAD:TenentId"]}";
+                });
             services.AddControllers();
         }
 
